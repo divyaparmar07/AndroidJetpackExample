ed3da9b0fe303ace309d9279b5ecba1a6ea675d3
Divya Parmar <divyaparmar0207@gmail.com>
2024-02-25 20:02:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/androidjetpackexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidjetpackexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
